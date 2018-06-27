7787c5f314aff17f69c61ebd6e09e21d976bb58e
eosuna <ernesto.osuna@demandforce.com>
2018-06-27 16:55:16 -0700

    composicion para alumnos agregada

--- src/reducer/listaAlumnos.js
+++ src/reducer/listaAlumnos.js
@@ -3,25 +3,48 @@ const initialState = {
   byId: [],
   byHash: {}
 }
+
+
+const initialAlumno = {
+  id: 0,
+  nombre:'',
+  asistencias: []
+  
+}
+const alumnos = (state= initialState, action) => {
+  switch (action.type) {
+    case grupos.AGREGAR_ALUMNOS: {
+      return state
+    }
+    case grupos.CAMBIAR_ALUMNO_ASISTENCIA: {
+
+      let newArr = Object.assign({}, state);
+      newArr.asistencias[action.index] = action.newValue
+      return newArr
+    }
+    default: 
+    return state;
+    
+  }
+}
+
 const listaAlumnos = (state = initialState, action) => {
   switch (action.type) {
     case grupos.AGREGAR_ALUMNO:
-      var newHash= Object.assign({}, state.byHash,{[action.id]: action.payload});
-      return {
-        byId: [ ...state.byId,action.id],
-        byHash: newHash
-      }
+      return state
     case grupos.AGREGAR_ALUMNOS:
       let newArr = Object.assign({}, state);
       newArr.byId = newArr.byId.concat(action.alumnos.map( (item, index) => {
-        newArr.byHash[item.id] = item
-        return item.id
+        const nuevo_alumno = alumnos(item,action)
+        newArr.byHash[nuevo_alumno.id] = nuevo_alumno
+        return nuevo_alumno.id
       }))
       return newArr
     case grupos.CAMBIAR_ALUMNO_ASISTENCIA: {
       let newArr = Object.assign({}, state);
-      newArr.byHash[action.indexAlumno].asistencias[action.index] = action.newValue
+      newArr.byHash[action.indexAlumno] = alumnos(newArr.byHash[action.indexAlumno],action)
       return newArr
+      
     }
     default:
       return state
