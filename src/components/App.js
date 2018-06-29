c882ab545a327c46b736d6f5ac2a20144177b7cd
eosuna <ernesto.osuna@demandforce.com>
2018-06-28 20:58:20 -0700

    grupos agregados

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,29 @@
 import React from "react"
 import { connect } from 'react-redux'
 import ListaAlumnos from './ListaAlumnos'
+import ListaGrupos from './ListaGrupos'
+import Header from  './Header'
 import SettingsCog from './SettingsCog'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCog } from '@fortawesome/free-solid-svg-icons'
+import { faCog , faChevronLeft  } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCog)
+library.add(faCog,faChevronLeft)
 
 class App extends React.Component {
 
   render() {
+    let body = null
+    if ( this.props.settings.mostrarGrupo !== 0 ){
+      body = <ListaAlumnos grupoId={this.props.settings.mostrarGrupo} />
+    }else { 
+      body = <ListaGrupos />
+    }
     return <React.Fragment>
       <SettingsCog />
-      <ListaAlumnos grupoId={1} />
+      <Header />
+      {body}
+      
       </React.Fragment>
     
   }
@@ -21,6 +31,7 @@ class App extends React.Component {
 
 const mapStateToProps = state => {
   return {
+    settings: state.settings
   }
 }
 const mapDispatchToProps = dispatch => {
