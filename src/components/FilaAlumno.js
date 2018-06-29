ebc30526ac13ebe30cb07234a13373890b620392
Ernesto Osuna <ernesto.osuna.garzon@gmail.com>
2018-06-28 21:01:24 -0700

    Revert "Lista grupos"

--- src/components/FilaAlumno.js
+++ src/components/FilaAlumno.js
@@ -1,40 +1,24 @@
 import React from "react";
-import AsistenciaInput from './AsistenciaInput'
 import style from './FilaAlumno.css'
 export default class FilaAlumno extends React.Component {
   constructor(props){
     super(props)
     this.renderDias = this.renderDias.bind(this)
-    this.handleCheckChange = this.handleCheckChange.bind(this)
+    
   }
-  handleCheckChange () {
-
-  }
-  renderDias () {
+  renderDias (){
+    console.log(this.props.numeroDeDias)
     let dias =[];
-    let countAsistio = 0
     for(let i = 0; i<this.props.numeroDeDias ;i++){
-      let asistio = false;
-      
-      if(this.props.alumno.asistencias[i]) {
-        asistio = true;
-        countAsistio++
-      }
-      dias.push(
-        <td key={i} className={style.bodyDias}>
-          <AsistenciaInput  grupoId={this.props.grupoId} checked={asistio} indexAlumno={this.props.alumno.id} index={i} tabIndex={i * this.props.countPadding + this.props.index+1} />
-        </td>)
+      dias.push(<td key={i} className={style.bodyDias}><input type='checkbox' tabIndex={i * this.props.countPadding + this.props.index}/></td>)
     }
-    dias.push(<td key={'final'}>
-      {(countAsistio / this.props.numeroDeDias*100).toFixed(2)}%
-    </td>)
     return dias
   }
   render() {
     
     return (
       <tr>
-        <td className={style.bNombre}>{this.props.alumno.nombre}</td>
+        <td className={style.bNombre}>{this.props.nombre}</td>
         {this.renderDias()}
       </tr>
       )
--- src/reducer/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-import grupos from './grupos'
-import settings from './settings'
-export default combineReducers({
-  grupos,
-  settings
-})
